b201fc81acf09914cadbc84f29f216f89ab25406
TrustedSec <davek@trustedsec.com>
2013-10-30 17:57:47 -0400

    Updated sign.sh

--- src/webattack/java_applet/sign.sh
+++ src/webattack/java_applet/sign.sh
@@ -1,4 +1,5 @@
 cp ../../html/unsigned/unsigned.jar Java_Exploit.jar
+jar ufm Java_Exploit.jar manifest.mf
 jarsigner -storetype pkcs12 -keystore /root/certs/MyCert.pfx Java_Exploit.jar "1"
 cp Java_Exploit.jar Signed_Update.jar.orig
 cp Java_Exploit.jar ../../html/Signed_Update.jar.orig
