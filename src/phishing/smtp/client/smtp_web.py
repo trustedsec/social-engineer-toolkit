9611eb13e573406f42817610147e5a3af4fc005b
Brian Berg <xexzy.cheddar@gmail.com>
2016-02-18 16:14:56 -0600

    New to github...Fixed issues with hotmail and Yahoo email providers. Current issue - Gmail does not work properly.

--- src/phishing/smtp/client/smtp_web.py
+++ src/phishing/smtp/client/smtp_web.py
@@ -86,13 +86,13 @@ for line in sendmail_file:
         if email_provider == "yahoo":
             if sendmail == 0:
                 smtp = ("smtp.mail.yahoo.com")
-                port = ("25")
+                port = ("587")
 
         # support smtp for hotmail
         if email_provider == "hotmail":
             if sendmail == 0:
-                smtp = ("smtp.hotmail.com")
-                port = ("25")
+                smtp = ("smtp.live.com")
+                port = ("587")
 
 
 print ("""
@@ -290,7 +290,7 @@ def mail(to, subject, prioflag1, prioflag2, text):
 
     if sendmail == 0:
 
-        if email_provider == "gmail":
+        if email_provider == "gmail" or email_provider == "yahoo" or email_provider == "hotmail":
             try:
                 mailServer.starttls()
             except:
