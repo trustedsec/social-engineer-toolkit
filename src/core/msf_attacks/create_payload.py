512beeee8e32dcb76ce100f9a4b7619b48888bbe
TrustedSec <davek@trustedsec.com>
2016-04-23 21:09:42 -0400

    reduced counter on client side generation

--- src/core/msf_attacks/create_payload.py
+++ src/core/msf_attacks/create_payload.py
@@ -193,7 +193,7 @@ if exploit_counter == 0:
         a = 1
 	counter = 0
         while a == 1:
-	    if counter == 15: 
+	    if counter == 10: 
 		print_error("Unable to generate PDF - there appears to be an issue with your Metasploit install.")
 		print_error("You will need to troubleshoot Metasploit manually and try generating a PDF. You can manually troubleshoot by going to /root/.set/ and typing msfconsole -r template.rc to reproduce the issue.")
 		pause = raw_input("Press {return} to move back.")
