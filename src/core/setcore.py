464da3a1d25d171a9fbacf442eca50b1e7bdebc6
TrustedSec <info@trustedsec.com>
2016-06-22 21:23:22 -0400

    update the base version and name

--- src/core/setcore.py
+++ src/core/setcore.py
@@ -260,7 +260,7 @@ def print_error(message):
 
 
 def get_version():
-    define_version = '7.1.2'
+    define_version = '7.2'
     return define_version
 
 class create_menu:
@@ -892,8 +892,8 @@ def show_banner(define_version, graphic):
     print(bcolors.BLUE + """
 [---]        The Social-Engineer Toolkit (""" + bcolors.YELLOW + """SET""" + bcolors.BLUE + """)         [---]
 [---]        Created by:""" + bcolors.RED + """ David Kennedy """ + bcolors.BLUE + """(""" + bcolors.YELLOW + """ReL1K""" + bcolors.BLUE + """)         [---]
-[---]                Version: """ + bcolors.RED + """%s""" % (define_version) + bcolors.BLUE + """                    [---]
-[---]             Codename: '""" + bcolors.YELLOW + """Blue Steel""" + bcolors.ENDC + bcolors.BLUE + """'               [---]
+[---]                  Version: """ + bcolors.RED + """%s""" % (define_version) + bcolors.BLUE + """                    [---]
+[---]             Codename: '""" + bcolors.YELLOW + """Wine and Gold""" + bcolors.ENDC + bcolors.BLUE + """'            [---]
 [---]        Follow us on Twitter: """ + bcolors.PURPLE + """@TrustedSec""" + bcolors.BLUE + """         [---]
 [---]        Follow me on Twitter: """ + bcolors.PURPLE + """@HackingDave""" + bcolors.BLUE + """        [---]
 [---]       Homepage: """ + bcolors.YELLOW + """https://www.trustedsec.com""" + bcolors.BLUE + """       [---]
