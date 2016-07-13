d3079b98cb7b29e687dbea64ef844424b4799ed0
TrustedSec <info@trustedsec.com>
2016-07-13 15:27:01 -0400

    added fsociety logo

--- src/core/setcore.py
+++ src/core/setcore.py
@@ -946,7 +946,7 @@ def show_banner(define_version, graphic):
 
 
 def show_graphic():
-    menu = random.randrange(2, 14)
+    menu = random.randrange(2, 15)
     if menu == 2:
         print(bcolors.YELLOW + r"""
                  .--.  .--. .-----.
@@ -1130,7 +1130,7 @@ def show_graphic():
            |`;=====' =''     ``=  `-'     `=====''|
            |______________________________________|
 	''')
-
+    
     if menu == 13:
         print(bcolors.RED + r"""
                       ..:::::::::..
@@ -1155,6 +1155,39 @@ def show_graphic():
                  ``:::::::::::::::::::''
                       ``:::::::::''""" + bcolors.ENDC)
 
+
+    if menu == 14:
+        print(bcolors.BOLD + """
+        cddddddddddddddddddddddddddddddddddddddddddd;
+        0Mo..........':ldkO0KKXXKK0kxoc,..........kMd
+        0Ml......;d0WMMMMMMMMMMMMMMMMMMMWKx:......kMd
+        0Ml...cOWMMMMMMMMMMMMMMMMMMMMMMMMMMMWO:...kMd
+        0Ml.lNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNc.kMd
+        0MdKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0OMd
+        0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd
+        0MxcxWMMMMMNXXNMMMMMMMMMMMMMMMNXXNMMMMMWkcKMd
+        0Md..lMKo,.,'...:kWMMMMMMMNx;...',.;dXMl.'XMd
+        0Mx'.,O;dXMMMXl....:dWMNo;....oXMMMKd;0,.'KMd
+        0MO;.,NMWMMMMMMWk;...XMK...:OWMMMMMMWMN,.cNMd
+        0MxxNMX;KMMKdcclkWN0WMMMN0WNxc:lxXMMk;WMXdKMd
+        0MMMMMO;MMl.......KMXOMNkMk.......xMM.NMMMMMd
+        0MMMMMMXKoclddl;.oWMdkMN,MN:.:ldolcdXNMMMMMMd   
+        0MMMMMMWXMMMMMMMW0KdoNMMdox0MMMMMMMMXMMMMMMMd
+        0MMMMXc'WMMMMMMMMkcWMMMMMMkcMMMMMMMMN'lXMMMMd
+        0MMMd..cMMMMMMMMNdoKMMMMM0x:XMMMMMMMM:..kMMMd
+        0MM0....d0KKOd:.....c0Kx'.....:d0NX0l....NMMd
+        0MMO.....................................WMMd
+        0Mdkc...................................0kOMd
+        0Ml.:Ol;........';;.......;,........':oX:.kMd
+        0Ml..,WMMMMWWWo...';;:c::;'...:WWMMMMMW;..kMd
+        0Ml...dMMMMMMMMKl...........c0MMMMMMMMd...kMd
+        0Ml...cMMMMMMMMMMMXOxdddk0NMMMMMMMMMMM'...kMd
+        0Ml....KMMMMMMMMMMMMMMMMMMMMMMMMMMMMMO....kMd
+        0Ml.....OMMMMMMMMMMMMMMMMMMMMMMMMMMMK.....kMd
+        0Ml......:XMMMMMMMMMMMMMMMMMMMMMMMNl......kMd
+        0Ml........lXMMMMMMMMMMMMMMMMMMMKc........kMd
+        0Ml..........:KMMMMMMMMMMMMMMM0,..........kMd
+        oO:............xOOOx:'';dOOOOd............lOc""" + bcolors.ENDC)
 
 #
 # identify if set interactive shells are disabled
