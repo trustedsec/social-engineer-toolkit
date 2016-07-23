8e321385e75a4cf03b48c5f8a1a8fc99c99a5a13
TrustedSec <info@trustedsec.com>
2016-07-23 19:07:02 -0400

    space fix

--- src/sms/sms.py
+++ src/sms/sms.py
@@ -8,7 +8,7 @@ try:
     print("""\n        ----- The Social-Engineer Toolkit (SET) SMS Spoofing Attack Vector -----\n""")
     print("This attack vector relies upon a third party service called www.spoofmytextmessage.com. This is a third party service outside of the control from the Social-Engineer Toolkit. The fine folks over at spoofmytextmessage.com have provided an undocumented API for us to use in order to allow SET to perform the SMS spoofing. You will need to visit https://www.spoofmytextmessage.com and sign up for an account. They example multiple payment methods such as PayPal, Bitcoin, and many more options. Once you purchase your plan that you want, you will need to remember your email address and password used for the account. SET will then handle the rest.\n")
     print("In order for this to work you must have an account over at spoofmytextmessage.com\n")
-    print("Special thanks to Khalil @sehnaoui  for testing out the service for me and finding spoofmytextmessage.com\n")
+    print("Special thanks to Khalil @sehnaoui for testing out the service for me and finding spoofmytextmessage.com\n")
     print_error("DISCLAIMER: By submitting yes, you understand that you accept all terms and services from spoofmytextmessage.com and you are fully aware of your countries legal stance on SMS spoofing prior to performing any of these. By accepting yes you fully acknowledge these terms and will not use them for unlawful purposes.") 
     message = raw_input("\nDo you accept these terms (yes or no): ")
     if message == "yes": 
