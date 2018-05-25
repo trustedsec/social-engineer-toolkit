3b71bde2f21e34cc489f6871031abd96f93e0ae0
TrustedSec <info@trustedsec.com>
2018-05-25 10:18:51 -0400

    old test code removal, add better wording, remove old templates

--- src/core/set.py
+++ src/core/set.py
@@ -348,8 +348,28 @@ try:
                                                     ipaddr = grab_ipaddress()
 
                                 if attack_vector == "harvester" or attack_vector == "tabnabbing" or attack_vector == "webjacking":
-                                    print_info("This option is used for what IP the server will POST to.")
-                                    print_info("If you're using an external IP, use your external IP for this")
+                                    print("""
+-------------------------------------------------------------------------------
+--- * IMPORTANT * READ THIS BEFORE ENTERING IN THE IP ADDRESS * IMPORTANT * ---
+
+The way that this works is by cloning a site and looking for form fields to
+rewrite. If the POST fields are not usual methods for posting forms this 
+could fail. If it does, you can always save the HTML, rewrite the forms to
+be standard forms and use the "IMPORT" feature. Additionally, really 
+important:
+
+If you are using an EXTERNAL IP ADDRESS, you need to place the EXTERNAL
+IP address below, not your NAT address. Additionally, if you don't know
+basic networking concepts, and you have a private IP address, you will
+need to do port forwarding to your NAT IP address from your external IP
+address. A browser doesns't know how to communicate with a private IP
+address, so if you don't specify an external IP address if you are using
+this from an external perpective, it will not work. This isn't a SET issue
+this is how networking works.
+""")
+
+                                    #print_info("This option is used for what IP the server will POST to.")
+                                    #print_info("If you're using an external IP, use your external IP for this")
                                     try:
                                         rhost = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                                         rhost.connect(('google.com', 0))
--- src/html/templates/template.py
+++ src/html/templates/template.py
@@ -3,34 +3,49 @@
 import os
 import shutil
 
-import src.core.setcore as core
+from src.core.setcore import *
 
 # Py2/3 compatibility
 # Python3 renamed raw_input to input
-try:
-    input = raw_input
-except NameError:
-    pass
+try: input = raw_input
+except NameError: pass
 
-dest = "src/html/"
-url = ""
+dest = ("src/html/")
+url = ("")
 
-core.debug_msg(core.mod_name(), "entering src.html.templates.template'", 1)
+debug_msg(mod_name(), "entering src.html.templates.template'", 1)
 
 #
 # used for pre-defined templates
 #
 print("""
+--------------------------------------------------------
+             **** Important Information ****
+
+For templates, when a POST is initiated to harvest
+credentials, you will need a site for it to redirect.
+
+You can configure this option under:
+
+      /etc/setoolkit/set.config
+
+Edit this file, and change HARVESTER_REDIRECT and
+HARVESTER_URL to the sites you want to redirect to
+after it is posted. If you do not set these, then
+it will not redirect properly. This only goes for
+templates.
+
+--------------------------------------------------------""")
+
+print("""
   1. Java Required
   2. Google
-  3. Facebook
-  4. Twitter
-  5. Yahoo
+  3. Twitter
 """)
-choice = raw_input(core.setprompt(["2"], "Select a template"))
+choice = raw_input(setprompt(["2"], "Select a template"))
 
 if choice == "exit":
-    core.exit_set()
+    exit_set()
 
 # file used for nextpage in java applet attack
 
@@ -52,34 +67,20 @@ if choice == "2":
     shutil.copyfile("src/html/templates/google/index.template", "src/html/index.template")
     url = "http://www.google.com"
 
-# if facebook
-if choice == "3":
-    if os.path.isfile("src/html/index.template"):
-        os.remove("src/html/index.template")
-    shutil.copyfile("src/html/templates/facebook/index.template", "src/html/index.template")
-    url = "http://www.facebook.com"
-
 # if twitter
-if choice == "4":
+if choice == "3":
     if os.path.isfile("src/html/index.template"):
         os.remove("src/html/index.template")
     shutil.copyfile("src/html/templates/twitter/index.template", "src/html/index.template")
     url = "http://www.twitter.com"
 
-# if yahoo
-if choice == "5":
-    if os.path.isfile("src/html/index.template"):
-        os.remove("src/html/index.template")
-    shutil.copyfile("src/html/templates/yahoo/index.template", "src/html/index.template")
-    url = "http://mail.yahoo.com"
+if not os.path.isdir(os.path.join(userconfigpath, "web_clone")):
+    os.makedirs(os.path.join(userconfigpath, "web_clone/"))
+if os.path.isfile(os.path.join(userconfigpath, "web_clone/index.html")):
+    os.remove(os.path.join(userconfigpath, "web_clone/index.html"))
+shutil.copyfile("src/html/index.template", os.path.join(userconfigpath, "web_clone/index.html"))
 
-if not os.path.isdir(os.path.join(core.userconfigpath, "web_clone")):
-    os.makedirs(os.path.join(core.userconfigpath, "web_clone/"))
-if os.path.isfile(os.path.join(core.userconfigpath, "web_clone/index.html")):
-    os.remove(os.path.join(core.userconfigpath, "web_clone/index.html"))
-shutil.copyfile("src/html/index.template", os.path.join(core.userconfigpath, "web_clone/index.html"))
-
-with open(os.path.join(core.userconfigpath, "site.template"), 'w') as filewrite:
+with open(os.path.join(userconfigpath, "site.template"), 'w') as filewrite:
     filewrite.write("TEMPLATE=SELF\nURL={0}".format(url))
 
-core.debug_msg(core.mod_name(), "exiting src.html.templates.template'", 1)
+debug_msg(mod_name(), "exiting src.html.templates.template'", 1)
