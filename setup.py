bbe2a48ef3c1d61c75e9dd89a703de6954bc4b52
David Kennedy <david.kennedy@trustedsec.com>
2020-02-03 17:26:50 -0500

    add setup.py installer for non-standalone

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#
+# quick installer for SET
+#
+#
+import subprocess
+import os
+print("[*] Installing requirements.txt...")
+subprocess.Popen("pip3 install -r requirements.txt", shell=True).wait()
+print("[*] Installing setoolkit to /usr/share/setoolkit..")
+print os.getcwd()
+subprocess.Popen("mkdir /usr/share/setoolkit/;mkdir /etc/setoolkit/;cp -rf * /usr/share/setoolkit/;cp src/core/config.baseline /etc/setoolkit/set.config", shell=True).wait()
+print("[*] Creating launcher for setoolkit...")
+filewrite = open("/usr/local/bin/setoolkit", "w")
+filewrite.write("#!/bin/sh\ncd /usr/share/setoolkit\n./setoolkit")
+filewrite.close()
+print("[*] Done. Chmoding +x.... ")
+subprocess.Popen("chmod +x /usr/local/bin/setoolkit", shell=True).wait()
+print("[*] Finished. Run 'setoolkit' to start the Social Engineer Toolkit.")
