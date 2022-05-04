16a319685e078a83a8bac7aa83c02100d1924836
EZ <91787162+EZWorldEZ@users.noreply.github.com>
2022-05-04 10:33:38 +0430

    OSError: [Errno 22]
    Invalid argument: '/usr/share/setoolkit/src/core/setcore.py'

--- src/core/setcore.py
+++ src/core/setcore.py
@@ -1188,15 +1188,6 @@ XX   MMMMMMMMMMMMMMMMMMMMNo         oNNNNo         oNMMMMMMMMMMMMMMMMMMMM   XX
 XX                                                                          XX
 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-    .o88o.                               o8o                .
-    888 `"                               `"'              .o8
-   o888oo   .oooo.o  .ooooo.   .ooooo.  oooo   .ooooo.  .o888oo oooo    ooo
-    888    d88(  "8 d88' `88b d88' `"Y8 `888  d88' `88b   888    `88.  .8'
-    888    `"Y88b.  888   888 888        888  888ooo888   888     `88..8'
-    888    o.  )88b 888   888 888   .o8  888  888    .o   888 .    `888'
-   o888o   8""888P' `Y8bod8P' `Y8bod8P' o888o `Y8bod8P'   "888"      d8'
-                                                                .o...P'
-                                                                `XER0'
 """ + bcolors.ENDC)
 
 #
